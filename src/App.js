bc81db6e8dcdeb26471eedb4521c386f00db6967
Artemiy Vereshchinskiy <vereshchinsky@grafitec.ru>
2017-08-16 19:44:01 +0300

    Fetching data

--- src/App.js
+++ src/App.js
@@ -1,21 +1,65 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+
+class FetchDemo extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cart : [],
+      step: "Ваш заказ"
+    };
+  }
+
+  componentDidMount() {
+    var that = this;
+    var url = 'http://localhost:3001/cart';
+
+    axios.get(url)
+      .then(res => {
+        const cart = res.data.map(obj => obj.data);
+        this.setState({ cart });
+        console.log({cart})
+      });
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div  className="container">
+        <h1>{this.state.step}</h1>
+        <div className="row">
+        <table className="table table-striped table-responsive">
+            <thead>
+              <tr>
+                <th>img</th>
+                <th>Title</th>
+                <th>Price</th>
+                <th>count</th>
+              </tr>
+            </thead>
+            <tbody>
+            {this.state.cart.map(cartitem =>
+              <tr key={cartitem.id}>
+                <th><img src={cartitem.image} className="img-fluid rounded" width="50px"/></th>
+                <td>{cartitem.title}</td>
+                <td>{cartitem.price}</td>
+                <td>{cartitem.count}</td>
+              </tr>
+            )}
+            </tbody>
+          </table>
+
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+
       </div>
     );
   }
 }
 
-export default App;
+
+
+export default FetchDemo;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import FetchDemo from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<FetchDemo subreddit="reactjs"/>, document.getElementById('root'));
 registerServiceWorker();
