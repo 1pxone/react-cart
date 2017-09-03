27ad0c3581a1bea12607ff578ec90c71a97a48a3
Artemiy Vereshchinskiy <hlebbobulka@MacBook-Air-Artemiy.local>
2017-09-03 23:51:00 +0300

    editing addresses

--- src/components/Step3.js
+++ src/components/Step3.js
@@ -7,7 +7,7 @@ const newAdress = {
   type: "object",
   required: ["title"],
   properties: {
-    id: {type: "number", default: 4},
+    id: {type: "number", default: 5},
     isActive: {type: "boolean", default: false},
     title: {type: "string", title: "Название", default: "#" },
     country: {type: "string",title: "Страна", enum: ["Россия", "Казахстан", "Беларусь"],enumNames: ["Россия", "Казахстан", "Беларусь"],default: "Россия"},
@@ -28,39 +28,39 @@ const uiSchema = {
 
 const log = (type) => console.log.bind(console, type);
 
-(function getid(){
-  var url = 'http://localhost:3001/user';
-  axios.get(url)
-    .then(res => {
-        let addresses = res.data.addresses.length;
-        console.log(addresses);
-    });
-});
-
-
-
-
-const onSubmit = ({formData}) => {
-  var url = 'http://localhost:3001/user';
-  axios.get(url)
-    .then(res => {
-        let addresses = res.data.addresses;
-        addresses.push(formData);
-        axios.patch(url, {
-          "addresses": addresses
-        })
-    });
-};
-
 class Step3 extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      addresses : []
+      addresses : [],
+      schema: {
+        title: "Добавить новый адрес",
+        type: "object",
+        required: ["title"],
+        properties: {
+          id: {type: "number", default: 5},
+          isActive: {type: "boolean", default: false},
+          title: {type: "string", title: "Название", default: "#" },
+          country: {type: "string",title: "Страна", enum: ["Россия", "Казахстан", "Беларусь"],enumNames: ["Россия", "Казахстан", "Беларусь"],default: "Россия"},
+          city: {type: "string", title: "Город"},
+          address: {type: "string", title: "Адрес"},
+          postcode: {type: "string", title: "Индекс"},
+          additionalInfo: {type: "string", title: "Дополнительная информация"}
+        }
+      },
+      uiSchema:{
+        "additionalInfo": {
+          "ui:widget": "textarea"
+        },
+        isActive: {"ui:widget": "hidden"},
+        id: {"ui:widget": "hidden"}
+      },
+      formData:{}
     };
     this.addNewAddress = this.addNewAddress.bind(this);
     this.deleteAddress = this.deleteAddress.bind(this);
+    this.editAddress = this.editAddress.bind(this);
   }
 
   componentDidMount() {
@@ -68,20 +68,27 @@ class Step3 extends React.Component {
     axios.get(url)
       .then(res => {
         if( res.data.addresses.length > 0){
+          let addressesCount = res.data.addresses.length;
+          // console.log(addressesCount);
           const addresses = res.data.addresses.map(obj => obj);
-          this.setState({ addresses});
+          this.setState({addresses,schema: {...this.state.schema, properties: {...this.state.schema.properties, id:{type: "number", default: addressesCount}}}});
         }
       });
   }
 
-  addNewAddress(formData){
+  addNewAddress(data){
+    // console.log(data.formData);
     let arr = [...this.state.addresses];
-    let newAdresses = arr.push();
+    let newAdresses = arr.push(data.formData);
     var url = 'http://localhost:3001/user';
     axios.patch(url, {
       "addresses": arr
-    })
-  }
+    }).then(res => {
+      let addressesCount = res.data.addresses.length;
+        const addresses = res.data.addresses.map(obj => obj);
+        this.setState({addresses,schema: {...this.state.schema, properties: {...this.state.schema.properties, id:{type: "number", default: addressesCount}}}});
+    });
+  };
 
 
   deleteAddress(id){
@@ -91,20 +98,29 @@ class Step3 extends React.Component {
     var url = 'http://localhost:3001/user';
     axios.patch(url, {
       "addresses": updatedAddresses
-    })
-    .then(function (response) {
-
-    })
-    .catch(function (error) {
-
+    }).then(res => {
+        const addresses = res.data.addresses.map(obj => obj);
+        this.setState({addresses, schema:this.state.schema,uiSchema:this.state.uiSchema});
     });
+  }
+
+  editAddress(id){
+    // let targetAddress = this.state.addresses.filter(function(address) {
+    //     return address.id == id;
+    // });
+    var url = 'http://localhost:3001/user';
+    // console.log(this.state.addresses[id]);
+    let targetData = this.state.addresses[id];
+    // saveTargetAddress(this.state.addresses[id])
+    this.setState({formData: targetData});
+
   }
 
   render() {
     if(this.state.addresses.length < 1){
       return (
-        <div  className="row py-5">
-          <div className="col-12">
+        <div  className="row py-5 justify-content-center">
+          <div className="col-12 text-left">
             <h1>{this.props.heading}</h1>
           </div>
           <div className="col">
@@ -144,7 +160,7 @@ class Step3 extends React.Component {
                   <p className="card-text">{address.address}, {address.postcode}</p>
                   <p className="card-text">{address.additionalInfo}</p>
                   <div className="row justify-content-around " >
-                    <button type="button" className="btn btn-outline-primary">Редактировать</button>
+                    <button onClick={() => this.editAddress(address.id)} type="button" className="btn btn-outline-primary">Редактировать</button>
                     <button onClick={() => this.deleteAddress(address.id)} className="btn btn-outline-danger">Удалить</button>
                   </div>
                 </div>
@@ -153,10 +169,11 @@ class Step3 extends React.Component {
           </div>
           <div className="col">
             <Form
-            schema={newAdress}
-            uiSchema={uiSchema}
+            schema={this.state.schema}
+            uiSchema={this.state.uiSchema}
             onChange={log("changed")}
-            onSubmit={onSubmit}
+            onSubmit={this.addNewAddress}
+            formData={this.state.formData}
             onError={log("errors")}>
               <div>
                 <button type="submit" className="btn btn-primary">Submit</button>
